a9e953fb23fa18e010d2b47b0d5d408bc28b7e7d
Darby Biddy <darbybiddy3@gmail.com>
2022-10-25 14:06:15 -0500

    added port

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const { animals } = require("./data/animals");
+
+const PORT = process.env.PORT || 3001
 const app = express();
 
 app.get("/api/animals", (req, res) => {
   res.json(animals);
 });
-app.listen(3001, () => {
-  console.log(`API server now on port 3001!`);
+app.listen(PORT, () => {
+  console.log(`API server now on port ${PORT}!`);
 });
 
 function filterByQuery(query, animalsArray) {
